738ea12da75482994c870d8b7dc8264cb8a1bda9
srikanth <srikanth2@gmail.com>
2021-02-03 07:55:20 +0530

    priya created a class

---
+++ WebApiGitHubProgram/Controllers/AsyncAwaitController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiGitHubProgram.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AsyncAwaitController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<int> Method1()
+        {
+            Console.WriteLine("Now I am in Method1 {0}", DateTime.Now.ToString());
+            await Method2();
+            Console.WriteLine("Now I am going into Method2 call{0}", DateTime.Now.ToString());
+            await Task.CompletedTask;
+            return 10;
+        }
+        private async Task Method2()
+        {
+            //Therd.Sleep(10000);
+            await Task.Delay(10000);
+            Console.WriteLine("Now I am going out from Method2 { 0} ", DateTime.Now.ToString());
+            //return 10;
+        }
+
+    }
+}
---
+++ WebApiGitHubProgram/Controllers/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiGitHubProgram.Controllers
+{
+    
+    
+		public class Program
+		{
+			public static void Main()
+			{
+			IList<Student> studentList = new List<Student>()
+				{
+			new Student() { StudentID = 1, StudentName = "priya", Age = 13 } ,
+			new Student() { StudentID = 2, StudentName = "teena",  Age = 21 } ,
+			new Student() { StudentID = 3, StudentName = "pooja",  Age = 18 } ,
+			new Student() { StudentID = 4, StudentName = "Ram" , Age = 22} ,
+			new Student() { StudentID = 5, StudentName = "srii" , Age = 27 }
+		};
+				
+				var avgAge = studentList.Average(s => s.Age);
+				var totalStudent = studentList.Count();
+				var oldest = studentList.Max( s =>s.Age);
+				var sumofAge = studentList.Sum( s =>s.Age);
+
+
+				Console.WriteLine("Average Age of Student: {0}", avgAge);
+				Console.WriteLine("Total Students: {0}", totalStudent);
+				Console.WriteLine("Oldest Student Age: {0}", oldest);
+				Console.WriteLine("sum of allStudent's Age: {0}", sumofAge);
+			Console.ReadLine();
+
+
+			}
+		}
+
+		public class Student
+		{
+
+			public int StudentID { get; set; }
+			public string StudentName { get; set; }
+			public int Age { get; set; }
+		}
+}
