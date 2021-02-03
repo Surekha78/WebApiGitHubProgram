c36e4b7916952a182497f9f7f17e5d987e9cecd0
Sue <sueicecream8@gmail.com>
2021-02-03 12:40:25 -0800

    Async Practice Program

---
+++ WebApiGitHubProgram/Controllers/AsyncPracticeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiGitHubProgram.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AsyncPracticeController : Controller
+    {
+        public async Task<long> Task1()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine("Printing Task1 Value {0}", DateTime.UtcNow.ToString());
+            Task2();
+            Task3();
+            Console.WriteLine("Printing Task2 value{0}", DateTime.UtcNow.ToString());
+            var a = stopwatch.ElapsedMilliseconds;
+            await Task.CompletedTask;
+            return a;
+        }
+        public async Task Task2()
+        {
+            await Task.Delay(10000);
+            Console.WriteLine("Printing Task2 Value{0}", DateTime.UtcNow.ToString());
+        }
+        public async Task Task3()
+        {
+            await Task.Delay(10000);
+            Console.WriteLine("Printing Task3 Value{0}", DateTime.MinValue.ToLocalTime());
+        }
+
+    }
+}
---
+++ WebApiGitHubProgram/Data/MyData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiGitHubProgram.Data
+{
+    public class MyData
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required, StringLength(50), MinLength(3)]
+        public string DataName { get; set; }
+        public double Sal { get; set; }
+        public DateTime? DOB { get; set; }
+        public DateTime? JoiningDate { get; set; }
+    }
+}
