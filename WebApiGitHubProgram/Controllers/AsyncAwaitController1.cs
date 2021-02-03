58f478bb9797b3bc83238fdd18d66360ad906e9e
teena1999 <thirumaleshabhoosarapu@gmail.com>
2021-02-02 21:02:22 -0600

    I created a asynchawait controller

---
+++ WebApiGitHubProgram/Controllers/AsyncAwaitController1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiGitHubProgram.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AsyncAwaitController1 : ControllerBase
+    {
+        [HttpGet]
+        public async Task<int> Method1()
+        {
+            Console.WriteLine(" Now I am in Method1 {0}", DateTime.Now.ToString());
+            Method2();
+            Console.WriteLine("Now I am Going into Method2 call{0}", DateTime.Now.ToString());
+            await Task.CompletedTask;
+            return 10;
+        }
+        private async Task Method2()
+        {
+            //Thread.Sleep(10000);
+            await Task.Delay(10000);
+            Console.WriteLine("Now I am Going out From Method2 {0}", DateTime.Now.ToString());
+            //return 10;
+
+        }
+
+       
+    }
+}
