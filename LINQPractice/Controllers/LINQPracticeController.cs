3369128065ff489e0ecbcff31c5b8274cc82d2d4
Ramesh Menta <rksofts.in@gmail.com>
2021-02-12 11:55:25 +0530

    LINQ Practice

---
+++ LINQPractice/Controllers/LINQPracticeController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LINQPractice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LINQPracticeController : ControllerBase
+    {
+        private readonly DemodbContext _context;
+
+        public LINQPracticeController(DemodbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetLINQPractice()
+        {
+            // to get Generated SQL Statement
+            var q = _context.OrderTbl.AsQueryable();
+            var GeneratedSQL = q.ToQueryString(); // SELECT dbo.[c.Oid], c.CustomerId, c.Amount FROM Customers as c;
+            
+            // 1. SELECT * FROM Customers;
+
+            // Method Query
+            var am = _context.OrderTbl.ToList();
+
+            // Query Syntax
+            var aq = (from o in _context.OrderTbl select o).ToList();
+
+            // 2. SELECT Country FROM Customers;
+            var ocm = _context.OrderTbl.Select(s => s.Amount).ToList(); // List<int> = new List<int>();
+            var ocq = (from o in _context.OrderTbl select o.Amount).ToList();
+
+            // 3. SELECT Country,City,Name FROM Customers;
+            var omm = _context.OrderTbl.Select(s => new { s.Oid, s.Amount, s.CustomerId });
+            var omq = (from o in _context.OrderTbl select new { o.Oid, o.Amount, o.CustomerId }).ToList();
+
+            // 4. SELECT * FROM Customers WHERE Country='Mexico';
+            var owm = _context.DemoTbl
+                .Where(w => w.Name == "Mexico").ToList();
+
+            var owq = from demotbl in _context.DemoTbl
+                      where demotbl.Name == "Mexico"
+                      select demotbl;
+
+            //5. SELECT * FROM Customers WHERE Country = 'Mexico' ORDER BY Country DESC;
+            var om = _context.DemoTbl
+                .Where(w => w.Name == "Mexico")
+                .OrderBy(o => o.Id) // Asc
+                // .OrderByDescending(o1 => o1.Id) // Desc
+                .ToList();
+
+            var oq = from demotbl in _context.DemoTbl
+                     where demotbl.Name == "Mexico"
+                     // orderby demotbl.Id // Asc
+                     orderby demotbl.Id descending // DESC
+                     select demotbl;
+
+            // 6. SELECT Country FROM Customers ORDER BY Country DESC
+
+            var ocom = _context.DemoTbl
+                .Where(w => w.Name == "Mexico")
+                .OrderBy(o => o.Id) // Asc
+                .Select(s=>s.Name)
+                .ToList();
+
+            var ocoq = from demotbl in _context.DemoTbl
+                       where demotbl.Name == "Mexico"
+                       orderby demotbl.Id // Asc
+                       select demotbl.Name;
+
+            /* 7. SELECT COUNT(CustomerID), Country
+                        FROM Customers
+                                GROUP BY Country
+                                HAVING COUNT(CustomerID) > 5;*/
+
+            var ohm = (_context.DemoTbl.ToList()).Where(w => w.Name == "Mexico").ToList();
+            var ohq = from x in (from d in _context.DemoTbl select d)
+                      where x.Name == "Mexico"
+                      select x;
+
+            // 8. Select with Groupby Clause 
+            /*SELECT COUNT(CustomerID), Country
+            FROM Customers
+            GROUP BY Country;*/
+
+            var dgm = _context.DemoTbl
+                .GroupBy(g => new { g.Id, g.Name })
+                .ToList();
+
+            var dgq = from d in _context.DemoTbl
+                      group d by new
+                      {
+                          d.Id,
+                          d.Name
+                      } into x
+                      select new
+                      {
+                          x.Key.Id,
+                          x.Key.Name,
+                          count = x.Count()
+                      };
+
+
+
+
+
+
+
+
+
+
+
+return Ok();
+}
+}
+}
---
+++ LINQPractice/Controllers/OrderTblsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LINQPractice;
+
+namespace LINQPractice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderTblsController : ControllerBase
+    {
+        private readonly DemodbContext _context;
+
+        public OrderTblsController(DemodbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/OrderTbls
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderTbl>>> GetOrderTbl()
+        {
+            return await _context.OrderTbl.ToListAsync();
+        }
+
+        // GET: api/OrderTbls/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderTbl>> GetOrderTbl(int id)
+        {
+            var orderTbl = await _context.OrderTbl.FindAsync(id);
+
+            if (orderTbl == null)
+            {
+                return NotFound();
+            }
+
+            return orderTbl;
+        }
+
+        // PUT: api/OrderTbls/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrderTbl(int id, OrderTbl orderTbl)
+        {
+            if (id != orderTbl.Oid)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(orderTbl).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderTblExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/OrderTbls
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<OrderTbl>> PostOrderTbl(OrderTbl orderTbl)
+        {
+            _context.OrderTbl.Add(orderTbl);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (OrderTblExists(orderTbl.Oid))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetOrderTbl", new { id = orderTbl.Oid }, orderTbl);
+        }
+
+        // DELETE: api/OrderTbls/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<OrderTbl>> DeleteOrderTbl(int id)
+        {
+            var orderTbl = await _context.OrderTbl.FindAsync(id);
+            if (orderTbl == null)
+            {
+                return NotFound();
+            }
+
+            _context.OrderTbl.Remove(orderTbl);
+            await _context.SaveChangesAsync();
+
+            return orderTbl;
+        }
+
+        private bool OrderTblExists(int id)
+        {
+            return _context.OrderTbl.Any(e => e.Oid == id);
+        }
+    }
+}
