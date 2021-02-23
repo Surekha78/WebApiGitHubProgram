175d5e3c47825060cc7af20043944ea0319eb737
Sue <sueicecream8@gmail.com>
2021-02-22 21:25:09 -0800

    merging

--- LINQPractice/DemodbContext.cs
+++ LINQPractice/DemodbContext.cs
@@ -26,7 +26,7 @@ namespace LINQPractice
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+#warning// To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Data Source=DESKTOP-2S22BSP\\SQLEXPRESS;Initial Catalog=Demodb;Integrated Security=True;");
             }
         }
